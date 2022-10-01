97d2f100e4a412d3adb146131f9422917cfcffce
Mohammad Hamzah Iqbal <mohammad.iqbal.7@city.ac.uk>
2022-10-01 21:22:39 +0100

    products filter working v1

--- Components/Header.js
+++ Components/Header.js
@@ -14,7 +14,6 @@ const Header = () => {
       name: 'HomePage',
       Link:'/'
       
-
     }
     ,
     {
--- pages/category/all.js
+++ pages/category/all.js
@@ -11,68 +11,15 @@ import { Product } from "../../Components/category/ProductCard"
 
 
 function About({ products }) {
-  
+
   const [filters, updateFilters] = useState(true)
-  const [category, updateCategory] = useState('all')
-  const [color, updateColor] = useState('')
+  const [type, updatetype] = useState('')
   const [size, updateSizes] = useState('')
   const [isMobile, setMobile] = useState()
 
-  const categories = ['all', 'Flags', 'Cartoon']
-  const colors = ['red', 'white', 'green', 'blue']
-  const sizes = ['small', 'large']
+  const types = ['flags', 'cartoon']
 
-
-
-  function SecondaryHeader() {
-    return (
-      <div className="bg-red-300 w-full h-14 flex justify-between items-center sticky top-0 z-20" id='secondaryHeader'>
-        <div className="ml-5 text-lg font-bold">
-          {/* {category} ({products.length}) */}
-        </div>
-        <div className="flex mr-5">
-          <p onClick={() => updateFilters(!filters)} className="mr-12">{filters ? 'Hide' : 'Show'} Filters</p>
-          <p>Sort By</p>
-        </div>
-      </div>
-    )
-  }
-
-  function Filters() {
-
-    return (
-      <div className="flex flex-col text-center sticky top-10" >
-        {/* reset button */}
-        <div className="mt-5">
-          <p onClick={() => {
-            updateColor('')
-            updateSizes('')
-          }}>Reset</p>
-        </div>
-
-        <div className="mt-5">
-          {categories.map((category, index) => {
-            return <p onClick={() => updateCategory(category)} key={index}>{category}</p>
-          })}
-        </div>
-
-        <div className="mt-20">
-          <p>Color</p>
-          {colors.map((color, index) => {
-            return <p onClick={() => updateColor(color)} key={index}>{color}</p>
-          })}
-        </div>
-
-        <div className="mt-20">
-          <p>sizes</p>
-          {sizes.map((size, index) => {
-            return <p onClick={() => updateSizes(size)} key={index}>{size}</p>
-          })}
-        </div>
-
-      </div>
-    )
-  }
+  const sizes = ['small', 'large', 'medium']
 
   useEffect(() => {
     resize()
@@ -91,9 +38,77 @@ function About({ products }) {
 
 
 
+  function SecondaryHeader() {
+    return (
+      <div className="bg-red-300 w-full h-14 flex justify-between items-center sticky top-0 z-30" id='secondaryHeader'>
+        <div className="ml-5 text-lg font-bold">
+          {/* {type} ({products.length}) */}
+        </div>
+        <div className="flex mr-5">
+          <p onClick={() => updateFilters(!filters)} className="mr-12">{filters ? 'Hide' : 'Show'} Filters</p>
+          <p>Sort By</p>
+        </div>
+      </div>
+    )
+  }
+
+  function Filters() {
+
+    return (
+      <div className="flex flex-col text-center sticky top-10" >
+        {/* reset button */}
+        <div className="mt-5">
+          <p onClick={() => {
+            updatetype('')
+            updateSizes('')
+          }}>Reset</p>
+        </div>
+
+        <div className="mt-5">
+          {types.map((typeLocal, index) => {
+            return (
+              <div key={index} className='flex justify-center'>
+                <p onClick={() => updatetype(typeLocal)} className='inline'>{typeLocal}</p>
+                {type === typeLocal ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block ml-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+
+                ) : ''}
+
+              </div>)
+          })}
+        </div>
+
+
+        <div className="mt-20">
+
+          {sizes.map((sizeLocal, index) => {
+            return (
+              <div key={index} className='flex justify-center'>
+                <p onClick={() => updateSizes(sizeLocal)} key={index}>{sizeLocal}</p>
+                {size === sizeLocal ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block ml-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+
+                ) : ''}
+              </div>
+
+            )
+          })}
+
+        </div>
+
+      </div>
+    )
+  }
+
   return (
     <>
       <Header />
+      {type}
+      {size}
       <div className="w-screen h-full">
         {/* header for items */}
         <SecondaryHeader />
@@ -102,18 +117,22 @@ function About({ products }) {
           {/* remove products and display filters if on mobile screen */}
 
           {/* <div id='products' className=' w-full h-full' style={{ display: isMobile && filters ? 'none' : 'flex' }}> */}
-          <div id='products' className='flex flex-wrap w-full h-full justify-center'>
+          <div id='products' className={`flex flex-wrap w-full h-full justify-center ${isMobile && filters ? 'invisible':'visible'}`}>
             {
               products.map((product, index) => {
+                const metaData = product.metadata
 
-                return <Product product={product} key={index} />
+                return (metaData.type === type || metaData.size === size || (size === '' && type === '')) ? (
+                  <Product product={product} key={index} />
+                ) : ''
+
               })
             }
           </div>
 
           {/* display filters if true  */}
           {filters ? (
-            <div className="absolute w-full md:w-1/4 md:relative bg-slate-500">
+            <div className="z-10 absolute bg-red-400 h-screen w-full md:w-1/4 md:relative ">
               <Filters />
             </div>
 
@@ -129,12 +148,12 @@ function About({ products }) {
 export async function getServerSideProps() {
   const fetchProducts = await stripe.products.list();
   const fetchPrices = await stripe.prices.list();
-  
+
   let products = fetchProducts.data.map((product, index) => {
     return { ...product, ...fetchPrices.data[index], metadata: fetchProducts.data[index].metadata }
   })
-  
-  
+
+
   return {
     props: {
       products
