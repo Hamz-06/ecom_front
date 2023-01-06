f35dfe71539283e47ef39f0a6fc9fd11c4c93ad4
Mohammad Hamzah Iqbal <mohammad.iqbal.7@city.ac.uk>
2023-01-06 18:12:15 +0000

    env

--- util/stripe.js
+++ util/stripe.js
@@ -1,7 +1,8 @@
 
 import Stripe from 'stripe'
 
-
+console.log(process.env.NEXT_PUBLIC_STRIPE)
+console.log(`${process.env.NEXT_PUBLIC_STRIPE}`)
 const stripe = Stripe(process.env.NEXT_PUBLIC_STRIPE);
 
 
