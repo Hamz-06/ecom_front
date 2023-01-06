bcf9307fb5a4d314631f4edf4c90fe396ffd8ed0
Mohammad Hamzah Iqbal <mohammad.iqbal.7@city.ac.uk>
2023-01-06 04:47:13 +0000

    fixed imports

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { stripe } from "../../util/shopify";
+import { stripe } from "../../util/stripe";
 export default async function handler(req, res) {
   // console.log(req.method)
-  
+
   const items = JSON.parse(req.body)
   // res.send({lol:'lx'})
   if (req.method === 'POST') {
@@ -14,10 +14,10 @@ export default async function handler(req, res) {
         success_url: `${req.headers.origin}/cart/?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/cart/?session_id={CHECKOUT_SESSION_ID}`,
       });
-      
-      
+
+
       res.status(200).send(session)
-      
+
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
